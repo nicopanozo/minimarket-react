2c2a091755399b6a0c88f2bb5495aa06a6e2e49f
LuisGamboaS <63593597+legs30011@users.noreply.github.com>
2025-06-05 11:06:45 -0400

    feat(admin): integrate Redux products in AdminDashboard and support d… (#32)
    * feat(admin): integrate Redux products in AdminDashboard and support dark mode
    
    - Connected AdminDashboard to Redux state for live product rendering
    - Enhanced ProductCard and ProductList with dark mode styling
    - Cleaned up grid structure and fixed styling consistency
    
    * bu/format-toggle
    
    ---------
    
    Co-authored-by: EnriqueGamboa10 <enrique.gamboa@assuresoft.tech>

--- src/features/products/ProductList.tsx
+++ src/features/products/ProductList.tsx
@@ -8,11 +8,8 @@ const ProductList = ({ products }: ProductListProps) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full md: flex-1">
       {products.map((product) => {
-        return (
-          <ProductCard key={product.id} product={product} />
-        )
-      })
-      }
+        return <ProductCard key={product.id} product={product} />;
+      })}
     </div>
   );
 };
