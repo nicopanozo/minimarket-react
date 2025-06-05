86548ff35db3ba3c352d71e5afa8623f6bd6b9e9
mauricioarrequena <mauricio.ar.requena@gmail.com>
2025-06-05 05:42:04 -0400

    added the product list component to render the list of products

--- src/features/products/productsSlice.ts
+++ src/features/products/productsSlice.ts
@@ -1,29 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
-interface ProductsState {
-  products: any[];
-  loading: boolean;
-}
-
-const initialState: ProductsState = {
-  products: [],
-  loading: false,
+export type Product = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+  active: boolean;
+  categoryId: number;
 };
+
+type ProductsState = Product[];
+const initialState: ProductsState = [];
 
 const productsSlice = createSlice({
   name: "products",
   initialState,
   reducers: {
-    setProducts: (state, action: PayloadAction<any[]>) => {
-      state.products = action.payload;
-    },
-    setLoading: (state, action: PayloadAction<boolean>) => {
-      state.loading = action.payload;
+    setProducts(state, action: PayloadAction<Product[]>) {
+      return action.payload;
     },
   },
 });
 
-export const { setProducts, setLoading } = productsSlice.actions;
-export const productsReducer = productsSlice.reducer;
+export const { setProducts } = productsSlice.actions;
 export default productsSlice.reducer;
