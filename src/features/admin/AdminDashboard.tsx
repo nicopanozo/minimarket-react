aa1094028220cf9d39916671cd7bab31af3e2161
LuisGamboaS <63593597+legs30011@users.noreply.github.com>
2025-06-05 11:44:40 -0400

    fixed bug product list in admin dashboard (#35)
    Co-authored-by: EnriqueGamboa10 <enrique.gamboa@assuresoft.tech>

--- src/features/admin/AdminDashboard.tsx
+++ src/features/admin/AdminDashboard.tsx
@@ -1,20 +1,26 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import type { RootState } from "../../redux/store";
+import { useEffect } from "react";
+import { setProducts } from "../products/productsSlice";
+import { products as initialProducts } from "../../data/products";
 import OrderTable from "./OrderTable";
 import ProductForm from "./ProductForm";
 import ProductList from "../products/ProductList";
 
 const AdminDashboard = () => {
+  const dispatch = useDispatch();
   const products = useSelector((state: RootState) => state.products);
+
+  useEffect(() => {
+    if (products.length === 0) {
+      dispatch(setProducts(initialProducts));
+    }
+  }, [dispatch, products.length]);
 
   return (
     <div className="container-custom py-8 space-y-10">
-      <h2 className="text-2xl font-bold heading-dark">
-        Panel de Administración
-      </h2>
-
+      <h2 className="text-2xl font-bold heading-dark">Panel de Administración</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        {/* Bloque de productos */}
         <section>
           <h2 className="text-xl font-semibold heading-dark mb-4">Productos</h2>
           <ProductForm />
@@ -22,8 +28,6 @@ const AdminDashboard = () => {
             <ProductList products={products} />
           </div>
         </section>
-
-        {/* Bloque de pedidos */}
         <section>
           <h2 className="text-xl font-semibold heading-dark mb-4">Pedidos</h2>
           <OrderTable />
