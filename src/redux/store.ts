64cb7bdf81075c317a3b6c02cb6f76addfa8a898
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-06-06 12:04:13 -0400

    feature: add orderSlice to handle order info: payment, shipping, etc.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,6 +3,7 @@ import cartSlice from '../features/cart/cartSlice';
 import userSlice from '../features/user/userSlice';
 import adminSlice from '../features/admin/adminSlice';
 import productsSlice from '../features/products/productsSlice';
+import orderSlice from '../features/order/orderSlice';
 
 export const store = configureStore({
   reducer: {
@@ -10,6 +11,7 @@ export const store = configureStore({
     products: productsSlice,
     user: userSlice,
     admin: adminSlice,
+    order: orderSlice,
   },
 });
 
