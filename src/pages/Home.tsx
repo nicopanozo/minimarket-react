e7005c010c75535e81adfc223cc83e17fe905082
mauricioarrequena <mauricio.ar.requena@gmail.com>
2025-06-06 23:19:02 -0400

    fix: removed unecessary coments

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
+import ProductList from '../features/products/ProductList';
+import ProductFilter from '../features/products/ProductFilter';
 import { setProducts } from '../features/products/productsSlice';
 import { products as productsData } from '../data/products';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import type { AppDispatch, RootState } from '../redux/store';
-import ProductList from '../features/products/ProductList';
-import ProductFilter from '../features/products/ProductFilter';
 
 const Home: React.FC = () => {
   const products = useSelector((state: RootState) => state.products);
@@ -14,13 +14,6 @@ const Home: React.FC = () => {
     dispatch(setProducts(productsData));
   }, []);
 
-  {
-    /* <div class="border-2 border-red-500 p-2 mb-2">Red border</div>
-<div class="border-2 border-blue-500 p-2 mb-2">Blue border</div>
-<div class="border-2 border-green-500 p-2 mb-2">Green border</div>
-<div class="border-2 border-yellow-500 p-2 mb-2">Yellow border</div>
-<div class="border-2 border-purple-500 p-2 mb-2">Purple border</div> */
-  }
   return (
     <div
       id="home"
