2258df3ed6e328e1895561e37ba1728e396fb4e8
mauricioarroyoas <mauricio.arroyo@assuresoft.tech>
2025-06-08 15:50:34 -0400

    feat: added navigation to product datail page (#59)
    Co-authored-by: mauricioarrequena <mauricio.ar.requena@gmail.com>

--- src/features/products/ProductDetail.tsx
+++ src/features/products/ProductDetail.tsx
@@ -1,6 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import type { Product } from '../../types/Product';
+import { productsData } from '../../data/products';
 
 const ProductDetail: React.FC = () => {
+  const { productId } = useParams();
+  const [product, setProduct] = useState<Product | undefined | null>(undefined);
+  const [relatedProducts, setRelatedProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const getProduct = (id: number) => {
+      const product = productsData.find(p => p.id === id);
+      return product;
+    };
+
+    const getRelatedProducts = (categoryId: number) => {
+      const products = productsData.filter(p => p.categoryId === categoryId);
+      return products;
+    };
+
+    const foundProduct = getProduct(parseInt(productId!))!;
+    if (!foundProduct) {
+      setProduct(null);
+      setRelatedProducts([]);
+    } else {
+      const relatedProducs = getRelatedProducts(foundProduct.categoryId);
+      setProduct(foundProduct);
+      setRelatedProducts(relatedProducs);
+    }
+  }, [productId]);
+
+  if (product === undefined) {
+    return <p>Loading the product..................................</p>;
+  }
+
+  if (product === null) {
+    return (
+      <div className="ml-auto mr-auto w-max flex flex-col items-center p-6 text-gray-700 dark:text-gray-300">
+        <p className="text-lg font-bold">Product Not Found</p>
+        <p className="mb-3">Sorry, we couldn&apos;t find that product.</p>
+        <button className="px-3 py-1 bg-indigo-600 text-white rounded">
+          <Link to="/">Go Back</Link>
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div
       id="productDetailPage"
@@ -10,7 +55,7 @@ const ProductDetail: React.FC = () => {
         <div id="breadcrumbs__menu" className="mb-4 flex">
           <span className="text-sm text-gray-600 dark:text-gray-300">
             <span className="text-blue-600 dark:text-blue-400">Home</span> &gt;
-            Wireless Headphones
+            {product.name}
           </span>
         </div>
 
@@ -26,7 +71,7 @@ const ProductDetail: React.FC = () => {
               <img
                 alt="test image"
                 className="w-full h-full object-cover"
-                src="https://picsum.photos/seed/lamp/400/300"
+                src={product.imageUrl}
               />
             </div>
 
@@ -39,10 +84,10 @@ const ProductDetail: React.FC = () => {
                 className="flex flex-col gap-8 lg:flex-row lg:justify-between"
               >
                 <span className="text-xl font-semibold text-gray-800 dark:text-gray-100">
-                  Wireless Headphones
+                  {product.name}
                 </span>
                 <span className="text-xl font-semibold text-gray-900 dark:text-gray-100">
-                  $89.99
+                  ${product.price}
                 </span>
               </div>
 
@@ -74,8 +119,7 @@ const ProductDetail: React.FC = () => {
                   Description
                 </span>
                 <span className="text-sm text-gray-700 dark:text-gray-300">
-                  High-quality wireless headphones with noise cancellation and
-                  long battery life.
+                  {product.description}
                 </span>
               </div>
             </div>
@@ -90,34 +134,36 @@ const ProductDetail: React.FC = () => {
             id="recommendations__list"
             className="flex justify-evenly gap-4 overflow-hidden"
           >
-            {[1, 2, 3].map(() => {
+            {relatedProducts?.map(product => {
               return (
                 <div
-                  key={Math.random()}
+                  key={product.id}
                   id="card"
-                  className="flex flex-col border border-gray-300 dark:border-gray-700 rounded-xl overflow-hidden bg-white dark:bg-gray-800"
+                  className="flex flex-col border border-gray-300 dark:border-gray-700 rounded-xl overflow-hidden bg-white dark:bg-gray-800 hover:shadow-xl dark:hover:shadow-[0_0_16px_rgba(255,255,255,0.15)] transition-shadow duration-300"
                 >
-                  <div
-                    id="card__image"
-                    className="w-full h-48 bg-gray-100 dark:bg-gray-700"
-                  >
-                    <img
-                      alt="another test image"
-                      className="w-full h-full object-cover"
-                      src="https://picsum.photos/seed/lamp/400/300"
-                    />
-                  </div>
-                  <div id="card__data" className="flex flex-col gap-2 p-4">
-                    <span className="text-base font-medium text-gray-800 dark:text-gray-100">
-                      Smartphone
-                    </span>
-                    <span className="text-sm text-gray-600 dark:text-gray-300">
-                      $56.00
-                    </span>
-                    <button className="mt-2 w-full bg-indigo-600 hover:bg-indigo-700 text-white text-sm py-2 rounded-md transition">
-                      Add to Cart
-                    </button>
-                  </div>
+                  <Link to={`/products/${product.id}`}>
+                    <div
+                      id="card__image"
+                      className="w-full h-48 bg-gray-100 dark:bg-gray-700"
+                    >
+                      <img
+                        alt="another test image"
+                        className="w-full h-full object-cover"
+                        src={product.imageUrl}
+                      />
+                    </div>
+                    <div id="card__data" className="flex flex-col gap-2 p-4">
+                      <span className="text-base font-medium text-gray-800 dark:text-gray-100">
+                        {product.name}
+                      </span>
+                      <span className="text-sm text-gray-600 dark:text-gray-300">
+                        ${product.price}
+                      </span>
+                      <button className="mt-2 w-full bg-indigo-600 hover:bg-indigo-700 text-white text-sm py-2 rounded-md transition">
+                        Add to Cart
+                      </button>
+                    </div>
+                  </Link>
                 </div>
               );
             })}
